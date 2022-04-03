e55513ab3570ca583ddd90b46f089318ad0b7636
ooktsl <omerkutsal@gmail.com>
2022-04-03 15:21:20 +0200

    Update Proje6.sql

--- Proje6.sql
+++ Proje6.sql
@@ -1,1 +1,11 @@
 
+1)  SELECT ROUND(AVG(rental_rate),2) FROM film;
+
+2)  SELECT COUNT (DISTINCT(title)) FROM film
+    WHERE title ~~* 'C%';
+    
+3)  SELECT MAX (length) FROM film
+    WHERE rental_rate = 0.99;
+
+4)  SELECT COUNT (DISTINCT replacement_cost) FROM film
+    WHERE length > 150;
